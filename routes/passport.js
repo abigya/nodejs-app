38cd4429b5d88886cd5d5070c1a651a9b653eba5
Abigya Devkota <37885957+abigya@users.noreply.github.com>
2018-12-16 08:23:45 -0600

    Update passport.js

--- routes/passport.js
+++ routes/passport.js
@@ -6,7 +6,8 @@ var connection = mysql.createConnection({
   host     : '127.0.0.1',
   user     : 'root',
   password : 'Abigy@15',
-  database : 'smartfridge'
+  database : 'smartfridge',
+  port     :  3306
 
 
 });
